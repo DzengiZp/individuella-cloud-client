415f24f0ebd734d90914b570b272f89ccaf8af13
Dzengiz <dzengiz.prentic@hotmail.com>
2025-09-08 12:42:52 +0200

    Update API_ENDPOINTS to use VITE_API_KEY for REGISTER_VISITOR

--- src/api/apiEndpoints.ts
+++ src/api/apiEndpoints.ts
@@ -1,5 +1,3 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; //TODO: Test deployment string when hosting site.
-
 export const API_ENDPOINTS = {
-  REGISTER_VISITOR: `${API_BASE_URL}${import.meta.env.VITE_REGISTER_VISITOR}`,
+  REGISTER_VISITOR: `${import.meta.env.VITE_API_KEY}`,
 };
